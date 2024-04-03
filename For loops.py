168128bebed3183d0db2551b5753f3204b957c4e
PriyadharshiniManikanadan <145161399+PriyadharshiniManikanadan@users.noreply.github.com>
2024-04-03 09:38:53 -0400

    Update For loops.py

--- For loops.py
+++ For loops.py
@@ -16,7 +16,6 @@
 *  With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc
 *  The for loop does not require an indexing variable
   """
-
 #example
 colors =["red", "blue", "green", "yellow"]
 for x in colors:
@@ -70,7 +69,6 @@ for x in range(5):
   The else keyword in a for loop specifies a block of code to be executed when the loop is finished
  
  """
-
 for x in range(5):
      print(x)
 else:
